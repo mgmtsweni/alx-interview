0e18a02a7f39ac1ca156adf92f1f15eb9354ceef
mgmtsweni <mtswenigift@gmail.com>
2023-01-27 12:21:33 +0200

    Pascal's Triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+from typing import list
+
+def pascal_triangle(n: int):
+    """
+        Returns a a list of lits reprisenting 
+        Pascal's triangle of a given integer.
+    """
+    triangle  = []
+    if n <= 0:
+        return triangle 
+    
+    for i in range(n):
+        level = [1]
+        for j in range(1, i + 1):
+            level.append(level[j - 1] * (i - j + 1)// j)
+        triangle .append(level)
+    return triangle 
