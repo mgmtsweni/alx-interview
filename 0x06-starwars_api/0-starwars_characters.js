a240200c8311e857f73631367301928bd7c27c2d
mgmtsweni <mtswenigift@gmail.com>
2023-03-09 21:06:39 +0200

    update

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,3 +1,26 @@
 #!/usr/bin/node
-
 // A script that prints all characters of a Star Wars movie
+
+const request = require('request');
+const args = process.argv[2];
+const movie_url = `https://swapi-api.alx-tools.com/api/films/${args}`;
+
+request(movie_url, function (error, response, body) {
+  if (!error) {
+    const movie = JSON.parse(body);
+    const characters = movie.characters;
+    printChar(characters, 0);
+  }
+});
+
+function printChar (url, index) {
+  const eachCharurl = url[index];
+  request(eachCharurl, function (error, response, body) {
+    if (!error) {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < url.length) {
+        printChar(url, index + 1);
+      }
+    }
+  });
+}
