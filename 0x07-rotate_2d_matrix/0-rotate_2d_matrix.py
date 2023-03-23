e400f18d048b34570eb32825ba711bf3a80abadf
mgmtsweni <mtswenigift@gmail.com>
2023-03-23 20:39:04 +0200

    update

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """ Rotate 90 Degrees Clockwise"""
-import itertools
 
 
 def rotate_2d_matrix(matrix):
@@ -8,7 +7,6 @@ def rotate_2d_matrix(matrix):
 
     n = len(matrix[0])
 
-    """ for i in range(n - 1, -1, -1):
-        for j in range(0, n):"""
-    for i, j in itertools.product(range(n - 1, -1, -1), range(n)):
-        matrix[j].append(matrix[i].pop(0))
+    for i in range(n - 1, -1, -1):
+        for j in range(0, n):
+            matrix[j].append(matrix[i].pop(0))
