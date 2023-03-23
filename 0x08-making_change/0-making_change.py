7fbc194eb84f233f54f5a27d38167dd747db8fc0
mgmtsweni <mtswenigift@gmail.com>
2023-03-23 20:21:09 +0200

    update

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -6,16 +6,15 @@ def makeChange(coins, total):
     """Return: fewest number of coins needed to meet total"""
     if total <= 0:
         return 0
-    rem = total
     count = 0
     index = 0
     sortedCoins = sorted(coins, reverse=True)
     i = len(coins)
-    while rem > 0:
+    while total > 0:
         if index >= i:
             return -1
-        if rem - sortedCoins[index] >= 0:
-            rem -= sortedCoins[index]
+        if total - sortedCoins[index] >= 0:
+            total -= sortedCoins[index]
             count += 1
         else:
             index += 1
