10dfd16579f58db1f2579f224b9a6dbc6bc82d1c
mgmtsweni <mtswenigift@gmail.com>
2023-04-21 22:54:03 +0200

    submit

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,81 +1,56 @@
 #!/usr/bin/python3
-"""N queens solution finder module."""
+""" Solution to the nqueens problem"""
 import sys
 
 
-solutions = []
-n = 0
-pos = None
+def backtrack(r, n, cols, positive, negative, board):
+    """ backtrack function to find solution"""
+    if r == n:
+        res = []
+        for l in range(len(board)):
+            res.extend([l, k] for k in range(len(board[l])) if board[l][k] == 1)
+        print(res)
+        return
+
+    for c in range(n):
+        if c in cols or (r + c) in positive or (r - c) in negative:
+            continue
+
+        cols.add(c)
+        positive.add(r + c)
+        negative.add(r - c)
+        board[r][c] = 1
+
+        backtrack(r+1, n, cols, positive, negative, board)
+
+        cols.remove(c)
+        positive.remove(r + c)
+        negative.remove(r - c)
+        board[r][c] = 0
 
 
-def get_input():
-    """Retrieves and validates this program's argument."""
-    global n
-    n = 0
-    if len(sys.argv) != 2:
+def nqueens(n):
+    """ Solution to nqueens problem"""
+    cols = set()
+    positive_diag = set()
+    negative_diag = set()
+    board = [[0] * n for _ in range(n)]
+
+    backtrack(0, n, cols, positive_diag, negative_diag, board)
+
+
+if __name__ == "__main__":
+    n = sys.argv
+    if len(n) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
     try:
-        n = int(sys.argv[1])
-    except Exception:
+        nn = int(n[1])
+        if nn < 4:
+            print("N must be at least 4")
+            sys.exit(1)
+        nqueens(nn)
+    except ValueError:
         print("N must be a number")
+
         sys.exit(1)
-    if n < 4:
-        print("N must be at least 4")
-        sys.exit(1)
-    return n
-
-
-def is_attacking(pos0, pos1):
-    """Checks if the positions of two queens are in an attacking mode."""
-    if (pos0[0] == pos1[0]) or (pos0[1] == pos1[1]):
-        return True
-    return abs(pos0[0] - pos1[0]) == abs(pos0[1] - pos1[1])
-
-
-def group_exists(group):
-    """Checks if a group exists in the list of solutions."""
-    global solutions
-    for stn in solutions:
-        i = 0
-        for stn_pos in stn:
-            for grp_pos in group:
-                if stn_pos[0] == grp_pos[0] and stn_pos[1] == grp_pos[1]:
-                    i += 1
-        if i == n:
-            return True
-    return False
-
-
-def build_solution(row, group):
-    """Builds a solution for the n queens problem."""
-    global solutions
-    global n
-    if row == n:
-        tmp0 = group.copy()
-        if not group_exists(tmp0):
-            solutions.append(tmp0)
-    else:
-        for col in range(n):
-            a = (row * n) + col
-            matches = zip([pos[a]] * len(group), group)
-            used_positions = map(lambda x: is_attacking(x[0], x[1]), matches)
-            group.append(pos[a].copy())
-            if not any(used_positions):
-                build_solution(row + 1, group)
-            group.pop(len(group) - 1)
-
-
-def get_solutions():
-    """Gets the solutions for the given chessboard size."""
-    global pos, n
-    pos = list(map(lambda x: [x // n, x % n], range(n ** 2)))
-    a = 0
-    group = []
-    build_solution(a, group)
-
-
-n = get_input()
-get_solutions()
-for solution in solutions:
-    print(solution)
