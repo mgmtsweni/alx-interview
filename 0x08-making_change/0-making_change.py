45bd690dbebebb66a672c0ea82ee0c39d8ebfeb6
mgmtsweni <mtswenigift@gmail.com>
2023-05-16 15:42:42 +0200

    New Chang Making

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""A Module to make change"""
+
+
+def makeChange(coins, total):
+    """Returns: fewest number of coins needed to meet total"""
+    if total <= 0:
+        return 0
+    count = 0
+    i = 0
+    sorted_Coins = sorted(coins, reverse=True)
+    l = len(coins)
+    while total > 0:
+        if i >= l:
+            return -1
+        if total - sorted_Coins[i] >= 0:
+            total -= sorted_Coins[i]
+            count+= 1
+        else:
+            i += 1
+    return count
